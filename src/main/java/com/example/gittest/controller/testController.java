5c29ab0cc2697a4acf3afb94ea718f86f02f1d7e
yanglin <1179595024@qq.com>
2019-12-23 11:19:54 +0800

    master-1

---
+++ src/main/java/com/example/gittest/controller/testController.java
@@ -0,0 +1,13 @@
+package com.example.gittest.controller;
+
+/**
+ * @author yanglin
+ * @date 2019/12/23
+ */
+public class testController {
+
+    public static void main(String[] args) {
+        String s = "master1";
+        System.out.println(s);
+    }
+}
