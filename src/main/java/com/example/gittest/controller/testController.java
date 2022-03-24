90701348c9d0c9096de80028164f6e21050845af
yanglin <1179595024@qq.com>
2022-03-24 14:18:39 +0800

    master

--- src/main/java/com/example/gittest/controller/testController.java
+++ src/main/java/com/example/gittest/controller/testController.java
@@ -7,7 +7,7 @@ package com.example.gittest.controller;
 public class testController {
 
     public static void main(String[] args) {
-        String s = "dev";
+        String s = "master";
         System.out.println(s);
     }
 }
